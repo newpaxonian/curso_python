bfa2ac8d3e772cf0a21f4fb49687f87324538ce6
newpaxonian <99375030+newpaxonian@users.noreply.github.com>
2022-03-22 01:59:20 -0500

    first update

---
+++ timeit_test.py
@@ -0,0 +1,29 @@
+import timeit
+import numpy
+
+def test1():
+    [x for x in range(1, 151) if not any([x % y == 0 for y in range(2, x)]) and not x == 1]
+    
+    
+def test2():
+    [x for x in range(2, 151) if not any([x % y == 0 for y in range(2, x)])]
+    
+    
+def test3():
+    # Initialize a list
+    primes = []
+    for possiblePrime in range(2, 151):
+    # Assume number is prime until shown it is not.
+        isPrime = True
+        for num in range(2, int(possiblePrime ** 0.5) + 1):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(possiblePrime)
+    
+
+
+print(timeit.timeit('test1()', globals=globals(), number=10))
+print(timeit.timeit('test2()', globals=globals(), number=10))
+print(timeit.timeit('test3()', globals=globals(), number=10))
